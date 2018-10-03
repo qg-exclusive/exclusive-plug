5487f0056d9f80fd186f8102ee82978ae242e90e
WilderGao <845956752@qq.com>
2018-10-03 22:51:32 +0800

    增加日志输出

--- src/main/java/com/qg/exclusiveplug/handlers/StringProtocolHandler.java
+++ src/main/java/com/qg/exclusiveplug/handlers/StringProtocolHandler.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 
 /**
  * @author WilderGao
